37c91d667f2d41cc2d37ed7d6fcb9c49fcf28758
Tu Nguyen <tun@nal.vn>
2022-06-21 08:59:15 +0700

    ok

--- src/Babylon/BasicScene.ts
+++ src/Babylon/BasicScene.ts
@@ -1,9 +1,12 @@
 import {
+  Color3,
+  Color4,
   Engine,
   FreeCamera,
   HemisphericLight,
   MeshBuilder,
   Scene,
+  StandardMaterial,
   Vector3,
 } from "@babylonjs/core";
 
@@ -13,9 +16,11 @@ export default class BasicScene {
   constructor(private canvas: HTMLCanvasElement) {
     this.engine = new Engine(canvas);
     this.scene = new Scene(this.engine);
+    this.scene.clearColor = new Color4(0.5, 0.5, 0.5);
+
     this.defineCamera();
     this.initLight();
-    this.initBall();
+    this.initPilot();
     this.engine.runRenderLoop(() => {
       this.scene.render();
     });
@@ -32,12 +37,35 @@ export default class BasicScene {
       new Vector3(0, 1, 0),
       this.scene
     );
-    light.intensity = 0.8;
   }
 
-  initBall() {
-    const ball = MeshBuilder.CreateSphere("ball", { diameter: 1 }, this.scene);
-    MeshBuilder.CreateGround("ground", { width: 10, height: 10 }, this.scene);
-    ball.position = new Vector3(0, 1, 0);
+  initPilot() {
+    const pilot = MeshBuilder.CreateCylinder(
+      "pilot",
+      {
+        height: 0.75,
+        diameterTop: 0.2,
+        diameterBottom: 0.5,
+        tessellation: 6,
+        subdivisions: 1,
+      },
+      this.scene
+    );
+    const greyMat = new StandardMaterial("grey", this.scene);
+    greyMat.emissiveColor = new Color3(0.2, 0.2, 0.2);
+    pilot.material = greyMat;
+
+    const arm = MeshBuilder.CreateBox(
+      "arm",
+      { height: 0.75, width: 0.3, depth: 0.1875 },
+      this.scene
+    );
+    arm.material = greyMat;
+    arm.position.x = 0.125;
+    arm.parent = pilot;
+    // rotation order
+    // pilot.rotation.z = Math.PI / 2;
+    // pilot.rotation.x = Math.PI / 2;
+    pilot.rotation.y = Math.PI / 2;
   }
 }
